11d1e5e61e6096ddecb5c45bd659e9bfed1f7d92
nbgao <nbgao@126.com>
2019-07-24 22:57:01 +0800

    Add RL environment (Gym & Mujoco)

---
+++ lab4/gym1.py
@@ -0,0 +1,12 @@
+import gym
+
+env = gym.make("CartPole-v1")
+observation = env.reset()
+for _ in range(10000):
+    env.render()
+    action = env.action_space.sample()
+    observaton, reward, done, info = env.step(action)
+    if done:
+        observation = env.reset()
+
+env.close()
